491b390def908656c950772d39afca894e5cd8e1
CozyBrian <dragonbrian33@gmail.com>
2022-08-11 01:17:24 +0000

    heroku database

--- src/services/postgres.js
+++ src/services/postgres.js
@@ -1,11 +1,8 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: "ec2-44-206-197-71.compute-1.amazonaws.com",
-  user: "uynvdddqoeufnk",
-  port: 5432,
-  password: 'a28a9622fcc308537fdef1118188bc74fc0c59433b060e5ef323c84b28bd8516',
-  database: 'd10i90k2edd3eq'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 module.exports = client;
