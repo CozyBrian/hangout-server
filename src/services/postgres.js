483d9fe1df23ae0d9275c2a85ea0ff155c6eeaa1
CozyBrian <dragonbrian33@gmail.com>
2022-08-13 04:18:26 +0000

    merge mistake

--- src/services/postgres.js
+++ src/services/postgres.js
@@ -1,8 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: 'tesla',
+  database: 'HangOut'
 });
 
 module.exports = client;
